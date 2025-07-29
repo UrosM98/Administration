1a2fa669d1eafe813572f409adb0157e4db514e7
UrosM <urosmarkovic102@gmail.com>
2025-07-29 12:23:33 +0200

    Classes Create

---
+++ AmbulanceHRAdministration/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdministrationAPI;
+
+namespace AmbulanceHRAdministration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static void SeedData(List<IEmployee> employees)
+        {
+            IEmployee doctor1 = new Doctor
+            {
+                
+            };
+        }
+    }
+
+    public class Doctor : EmployeeBase
+    {
+        
+    }
+
+    public class HeadOfDepartment : EmployeeBase
+    {
+
+    }
+
+    public class DeputyHeadMaster : EmployeeBase
+    {
+
+    }
+
+    public class HeadMaster : EmployeeBase
+    {
+
+    }
+}
